6d49a6725a3af54c30e6ad05c6cf5f569dc8cb60
Case Nelson <case@teammating.com>
2011-11-09 20:06:37 -0700

    final?

---
+++ README.md
@@ -0,0 +1,5 @@
+Playing with HTML5 Audio and Canvas processing.
+
+Webkit (Chrome) only
+
+Thanks to http://somethingemporium.com/2007/5/dev-audio-cat-half-tone for the idea
--- audioimage.js
+++ audioimage.js
@@ -1,4 +1,11 @@
-var audioContext = new webkitAudioContext();
+(function() {
+$(document).ready(function() {
+    setupCanvas();
+    setupAudio();
+    setupControls();
+    setupDrawing();
+});
+
 
 var canvas = null;
 var context = null; 
@@ -6,8 +13,12 @@ var cursor = null;
 
 var row = 0;
 var index = 0;
+
+var started = false;
+
 var readFromCanvas = function(n) {
     var output = new Float32Array(n);
+
     // we really only need as many as n pixels from index
 	var pixels = context.getImageData(0, 0, canvas.width, canvas.height);
 
@@ -15,50 +26,74 @@ var readFromCanvas = function(n) {
     var length = pixdata.length;
     var i = 0;
     while(i < n) {
-        var avg = pixdata[index] + pixdata[index+1] + pixdata[index+2];
-        avg = avg / 3;
-        output[i] = avg / 255;
-        index += 4;
+        output[i++] = 1 - (pixdata[index++]/255);
+        output[i++] = 1 - (pixdata[index++]/255);
+        output[i++] = 1 - (pixdata[index++]/255);
+
+        // skip alpha 
+        index++;
+
         index = (index % length);
         row = index / (4 * canvas.width);
         cursor.style.top = row + "px";
-
-        i++;
     }
 
     return output;
 }
 
-$(document).ready(function() {
-    cursor = document.getElementById('cursor');
+var processAudio = function(e) {
+    var left = e.outputBuffer.getChannelData(0);
+    var right = e.outputBuffer.getChannelData(1);
+    var n = e.outputBuffer.length;
+    var data = readFromCanvas(n);
+    left.set(data);
+    right.set(data);
+}
 
+var setupCanvas = function() {
+    cursor = document.getElementById('cursor');
     canvas = document.getElementById('canvas');
     context = canvas.getContext('2d');
+}
 
+var setupAudio = function() {
+    var audioContext = new webkitAudioContext();
     var src = audioContext.createJavaScriptNode(4096, 0, 1);
-    var phase = 0.0
-    var phaseInc = 440.0/audioContext.sampleRate
+    src.onaudioprocess = processAudio; 
+
     // It seems the audio context needs to warm up a bit longer - 
     // without the timeout the audio only plays a few loops
-    setTimeout(function() { 
-        src.onaudioprocess = function(e) {
-            var left = e.outputBuffer.getChannelData(0);
-            var right = e.outputBuffer.getChannelData(1);
-            var n = e.outputBuffer.length;
-            var data = readFromCanvas(n);
-            left.set(data);
-            right.set(data);
-        }
-        src.connect(audioContext.destination);       
-    }, 1000);
+    setTimeout(function() { src.connect(audioContext.destination); }, 1000);
+}
 
-    context.strokeStyle = 'rgb(220,220,220)';
+var setupControls = function() {
+    $('input[type="range"]').change(changeColor);
+    $('#clear').click(function() {
+        context.fillStyle = 'white';
+        context.fillRect(0,0,canvas.width,canvas.height);
+    });
+    changeColor();
+}
+
+var setupDrawing = function() {
     $('#canvas').mousemove(mouseMove);
-    $('#canvas').mouseup(mouseUp);
+    $(document).mouseup(mouseUp);
     $('#canvas').mousedown(mouseDown);
-});
+}
 
-var started = false;
+var changeColor = function() {
+    var h = $('#h').val();
+    var s = $('#s').val();
+    var l = $('#l').val();
+    var style = 'hsl('+h+','+s+'%,'+l+'%)';
+    var colorblock = document.getElementById('colorblock')
+    var colorCtx = colorblock.getContext('2d');
+    context.lineWidth = 10;
+    context.strokeStyle = style;
+    colorCtx.fillStyle = style;
+    colorCtx.fillRect(0,0,colorblock.width,colorblock.height);
+}
+
 var mouseDown = function(evt) {
     var x = evt.offsetX;
     var y = evt.offsetY;
@@ -81,3 +116,4 @@ var mouseUp = function(evt) {
     started = false; 
 }
 
+})();
